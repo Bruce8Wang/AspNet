47091f47074325a0bf3d0c9e4a853a02a7244904
Bruce8Wang <Bruce8Wang@qq.com>
2023-08-25 18:53:10 +0800

    导入windows service示例

---
+++ WinSrv/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Configuration;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace WinSrv
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			ServiceBase.Run(new WindowsService());
+		}
+		
+		public class WindowsService : ServiceBase
+		{
+			private Process process = null;
+			
+			protected override void OnStart(string[] args)
+			{
+				// 调用其他进程
+				string fileName = ConfigurationManager.AppSettings["fileName"] ?? "";
+				string arguments = ConfigurationManager.AppSettings["arguments"] ?? "";
+				process = Process.Start(fileName, arguments);
+			}
+
+			protected override void OnStop()
+			{
+				// 杀死其他进程
+				process.Kill();
+			}
+		}
+	}
+	
+}
