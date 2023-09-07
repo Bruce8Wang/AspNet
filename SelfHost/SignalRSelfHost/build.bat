d8185a84b2266386dc857e5d1afbd8ad5c93792e
Bruce8Wang <Bruce8Wang@qq.com>
2023-09-07 10:30:08 +0800

    Update build.bat

--- SelfHost/SignalRSelfHost/build.bat
+++ SelfHost/SignalRSelfHost/build.bat
@@ -1,2 +1,2 @@
-"..\..\nuget.exe" install packages.config -o ..\..\packages
+"..\nuget.exe" install packages.config -o ..\packages
 "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" -t:build -p:Configuration=Release
